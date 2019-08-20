934e70ad36da9870bba1273bee7b6bf106853183
jokerdada <wangbo@idotools.com>
2019-08-20 17:22:37 +0800

    。

---
+++ app/src/main/java/com/syido/design/adapterPattern/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package com.syido.design.adapterPattern;
+
+public interface AdvancedMediaPlayer {
+    public void playVlc(String fileName);
+    public void playMp4(String fileName);
+}
---
+++ app/src/main/java/com/syido/design/adapterPattern/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.syido.design.adapterPattern;
+
+public interface MediaPlayer {
+     void play(String type,String name);
+}
